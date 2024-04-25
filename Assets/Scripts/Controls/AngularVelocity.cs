e238a10bab9307ffb3ac4f5439d00ee4921542df
IvanPavlov <ivanpavlov7007@gmail.com>
2024-04-25 15:47:43 +0200

    Add comments

--- Assets/Scripts/Controls/AngularVelocity.cs
+++ Assets/Scripts/Controls/AngularVelocity.cs
@@ -21,15 +21,27 @@ public class AngularVelocity
         relativeAngleChange = 0f;
     }
 
+    /// <summary>
+    /// an update function for the angle/velocity calculation
+    /// </summary>
+    /// <param name="newDirection">new input direction</param> 
+    /// <param name="angularToLinearCoef">change in angle -> change in velocity. Degree to m/s</param>
+    /// <param name="lerpValue">smoothiness -> close to 0</param>
+    /// <param name="minBasicVelocity">velocity with no angle change</param>
+    /// <param name="deltaTime">pass it thorugh from Time.deltaTime</param>
+    /// <param name="maxAdditionalAngleVelocity">cap for the fastest velocity</param>
+    /// <param name="additionalVelocityGravity">how fast should @AngleChangeVelocity drop to 0</param>
     public void ChangeDirection(Vector2 newDirection, float angularToLinearCoef, float lerpValue, float minBasicVelocity, float deltaTime, float maxAdditionalAngleVelocity, float additionalVelocityGravity)
     {
         calculateRelativeAngleChange(newDirection, additionalVelocityGravity);
+        //determine how much velocity to add
         angleChangeVelocity = Mathf.Max(0f, Mathf.Min(maxAdditionalAngleVelocity, Mathf.LerpAngle(angleChangeVelocity + Vector2.Angle(lastDirection, newDirection), 0f, additionalVelocityGravity)));
         velocity = Vector2.Lerp( velocity, newDirection.normalized * Mathf.Max(minBasicVelocity, angleChangeVelocity * oneDegree * angularToLinearCoef), lerpValue);
         lastDirection = newDirection;
         characterMovement.Velocity = velocity;
     }
 
+    //How fast and in which direction angle changes. Little to no turn has values near 0
     private void calculateRelativeAngleChange(Vector2 newDirection, float additionalVelocityGravity)
     {
         relativeAngleChange = Mathf.Lerp(Vector2.SignedAngle(lastDirection, newDirection) + relativeAngleChange,0f,additionalVelocityGravity);
